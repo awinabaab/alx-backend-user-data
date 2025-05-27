29b4e8f5c5cc7a3aaafa91e8f3469e3c19274b38
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-05-27 11:46:07 +0000

    feat(0x01-Basic_authentication): add simple http api with basic authentication

---
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None if not getenv("AUTH_TYPE") else getenv("AUTH_TYPE")
+
+
+if auth:
+    if auth == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ Before request handler
+    """
+    if auth is None:
+        return
+
+    excluded_paths = [
+                      '/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/'
+                      ]
+
+    requires_auth = auth.require_auth(request.path, excluded_paths)
+    if not requires_auth:
+        return
+
+    authorization_header = auth.authorization_header(request)
+    if not authorization_header:
+        abort(401)
+
+    current_user = auth.current_user(request)
+    if not current_user:
+        abort(403)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
